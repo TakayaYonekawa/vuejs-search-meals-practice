e949eb2267b41303ffc27b68c0763da634456953
米川貴也 <zuomujianwu1@gmail.com>
2023-07-10 23:04:41 +0900

    入力欄作成

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import store from './store';
 
-createApp(App).use(router).mount("#app");  // use(router)を追記
+createApp(App).use(router).use(store).mount("#app");  // use(router)を追記
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,22 @@
 <template>
-    <h1>Home</h1>    
+  <div class="flex flex-col p-8 ">
+    <input
+      type="text"
+      class="rounded border-2 border-gray-200 w-full"
+      placeholder="Search for Meals"
+    />
+    <div class="flex justify-center gap-2 mt-2">
+        <router-link to="/" v-for="letter of letters" >
+        {{letter}}
+        </router-link>
+    </div>
+  </div>
 </template>
 
 <script setup>
+import { computed } from "vue";
+import store from "../store";
 
+const meals = computed(() => store.state.meals)
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split("")
 </script>
