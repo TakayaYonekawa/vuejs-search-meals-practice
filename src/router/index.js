b3606bd4d02b7ac9972c2b9b9fc7ae58ecc5ea2d
米川貴也 <zuomujianwu1@gmail.com>
2023-07-10 09:46:07 +0900

    routerエラー解決

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,17 @@
 import { createRouter, createWebHistory }  from 'vue-router'
-import Home from '../view/Home.vue'
+import Home from '../views/Home.vue'
 
 const routes = [
     {
-        path: '/',
-        name: 'home',
-        components: Home,
+        path: "/",
+        name: "",
+        component: Home,
     }
 ];
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
-})
+});
 
 export default router;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-    Home    
+    <h1>Home</h1>    
 </template>
 
 <script setup>
