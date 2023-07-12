ff42edd43d38eb52f964681e59aa5ca94a98454b
米川貴也 <zuomujianwu1@gmail.com>
2023-07-12 13:02:45 +0900

    レイアウト整える

---
+++ src/views/MealDetails.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="max-w-[800px] mx-auto p-8">
+    <h1 class="text-4xl font-bold mb-5 text-orange-500">{{ meal.strMeal }}</h1>
+    <img class="max-w-[100%]" :src="meal.strMealThumb" :alt="meal.strMeal" />
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
+import axiosClient from "../axiosCliend";
+
+const route = useRoute();
+const meal = ref([]);
+
+onMounted(() => {
+  axiosClient.get(`lookup.php?i=${route.params.id}`).then(({ data }) => {
+    debugger;
+    meal.value = data.meals[0] || {};
+  });
+});
+</script>
--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -10,25 +10,53 @@
   </div>
 
   <div>
-    <div v-for="meal of meals" :key="meal.idMeal" class="grid grid-cols-1 md:grid-cols-3 gap-5 px-8">
-        <img :src="meal.strMealThumb" :alt="strMeal">
-        <h3>{{meal.strMeal}}</h3>
-        <div>
-            <a :href="meal.strYoutube" target="_blank">Youtube</a>
-            <router-link to="/">View</router-link>
-        </div>
+    <div
+      v-for="meal of meals"
+      :key="meal.idMeal"
+      class="grid grid-cols-1 md:grid-cols-3 gap-5 px-8"
+    >
+    <router-link :to="{name: 'mealDetails', params: {id: meal.idMeal}}">
+      <img
+        :src="meal.strMealThumb"
+        :alt="strMeal"
+        class="rounded-t-xl w-full h-48 object-cover"
+      />
+    </router-link>
+      <h3 class="font-bold">{{ meal.strMeal }}</h3>
+      <div class="flex justify-between items-center">
+        <a
+          :href="meal.strYoutube"
+          target="_blank"
+          class="px-3 py-2 rounded border-2 text-white border-orange-600 bg-orange-500 hover:bg-orange-600 transition-colors"
+          >Youtube</a
+        >
+        <router-link
+          to="/"
+          class="px-3 py-2 rounded border-2 text-white border-orange-600 bg-orange-500 hover:bg-orange-600 transition-colors"
+          >View</router-link
+        >
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
 import { computed } from "@vue/reactivity";
-import {  ref } from "vue";
+import { onMounted, ref } from "vue";
 import store from "../store";
+import { useRoute } from "vue-router";
+const route = useRoute();
 const keyword = ref("");
 const meals = computed(() => store.state.searchedMeals);
 
 function searchMeals() {
-    store.dispatch('searchMeals', keyword.value)
+  store.dispatch("searchMeals", keyword.value);
 }
+
+onMounted(() => {
+  keyword.value = route.params.name
+  if(keyword.value){
+    searchMeals()
+  }
+})
 </script>
