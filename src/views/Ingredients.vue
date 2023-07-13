b590856836a5776eeabaa0151a66b892e2588b75
米川貴也 <zuomujianwu1@gmail.com>
2023-07-13 22:31:06 +0900

    検索欄追加

--- src/views/Ingredients.vue
+++ src/views/Ingredients.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="p-8">
     <h1 class="text-4xl font-bold mb-4 text-orange-500">Ingredients</h1>
+    <input
+      type="text"
+      v-model="keyword"
+      class="rounded border-2 border-gray-200 w-full"
+      placeholder="Search for Ingredients"
+    />
     <router-link
-      v-for="ingredient of ingredients"
+      v-for="ingredient of computedIngredients"
       :key="ingredient.idIngredient"
       :to="{
         name: 'byIngredient',
@@ -16,10 +22,21 @@
 </template>
 
 <script setup>
-import { onMounted, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
 import axiosClient from "../axiosClient";
 
+const keyword = ref("");
 const ingredients = ref([]);
+
+const computedIngredients = computed(() => {
+  if (!computedIngredients) return ingredients;
+  return ingredients.value.filter((i) => {
+    return (
+      (i.strIngredient || "")
+        .toLowerCase()
+        .includes(keyword.value.toLowerCase())     );
+  });
+});
 
 onMounted(() => {
   axiosClient.get("list.php?i=list").then(({ data }) => {
